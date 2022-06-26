6ab4d95738a7d4aebd9b11e0e9c9a98fa50a7909
ema <email@andrejschefer.de>
2022-06-26 10:12:36 +0200

    create jwt-secret

--- docker.sh
+++ docker.sh
@@ -12,7 +12,9 @@ POD=$(kubectl  get pod -n $NAME -o name)
 
 kubectl delete  $POD -n $NAME
 
-kubectl create secret generic db-user-pass   --from-file=./secrets/username.txt   --from-file=./secrets/password.txt --from-file=./secrets/db-host.txt --from-file=./secrets/db-name.txt  -n $NAME
+kubectl create secret generic db-user-pass --from-file=./secrets/username.txt   --from-file=./secrets/password.txt --from-file=./secrets/db-host.txt --from-file=./secrets/db-name.txt  -n $NAME
+
+kubectl create secret generic jwt-secret --from-file=./secrets/jwt_secret.txt -n $NAME
 
 kustomize build k3s/base/. | kubectl apply  -f -
 
